da72d0cdbd317b32c2beaf30f1fb9b27c4c01cfa
liangbing <909901234@qq.com>
2016-07-14 19:39:34 +0800

    Initial Commit

---
+++ XHTestAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHTestAnimationDemo
+//
+//  Created by liangbing on 16/7/14.
+//  Copyright © 2016年 liangxianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
